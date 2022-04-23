7bdb5bdb6883b4360e284e29fdc99c4c1821e368
PrajwalWebDev07 <prajwalwebdev07@gmail.com>
2022-04-23 13:28:26 +0530

    progress steps final code

---
+++ Progress_Steps/progress_steps.js
@@ -0,0 +1,39 @@
+
+let progress= document.querySelector('#progress')
+let prev= document.querySelector('#prev')
+let next= document.querySelector('#next')
+let circles= document.querySelectorAll('.circle')
+
+let currentActive=1;
+next.addEventListener('click',()=>{
+    currentActive++
+    if(currentActive>circles.length) currentActive=circles.length
+    updateProgress()
+})
+prev.addEventListener('click',()=>{
+    currentActive--
+    if(currentActive<1) currentActive=1
+    updateProgress()
+})
+
+function updateProgress(){
+    circles.forEach((circle,index)=>{
+        if(index<currentActive){
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    let actives=document.querySelectorAll('.active')
+    progress.style.width=(actives.length-1)/(circles.length-1)*100+'%'
+    if(actives.length===1) {
+        prev.disabled=true
+    }else if(actives.length===circles.length) {
+        next.disabled=true
+    }else {
+        next.disabled=false;
+        prev.disabled=false
+    }
+
+}
